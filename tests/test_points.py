127772bfa45d26af81d6c07abcfdfefe8cef2a01
Wolski <merito123@gmail.com>
2020-01-22 23:53:46 +0100

    Add bacis tests for get_points

--- tests/test_points.py
+++ tests/test_points.py
@@ -13,6 +13,18 @@ def test_get_get_point(client, elastic_mock):
 
 
 def test_post_get_point(client, elastic_mock):
-    elastic_mock.return_value.get_point.return_value = '3'
+    elastic_mock.return_value.get_point.return_value = {'a': '3'}
     res = client.post(url_for('points.get_point'), json={'id': 1})
     assert res.status_code == 200
+    assert res.json == {'a': '3'}
+
+
+def test_get_get_points(client, elastic_mock):
+    assert client.get(url_for('points.get_points')).status_code == 405
+
+
+def test_post_get_points(client, elastic_mock):
+    elastic_mock.return_value.get_points.return_value = {'a': '3'}
+    res = client.post(url_for('points.get_points'), json={'top_right': 1, 'bottom_left': 2})
+    assert res.status_code == 200
+    assert res.json == {'a': '3'}
