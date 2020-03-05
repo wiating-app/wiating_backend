d376b659a76724613edf1ca467b069d80a988119
Wolski <merito123@gmail.com>
2020-03-06 00:29:39 +0100

    Introduce class Point to cleanup point logic

---
+++ tests/test_elastic.py
@@ -0,0 +1,64 @@
+import pytest
+from wiating_backend.elastic import Point, NotDefined
+
+
+def test_createPoint():
+    point = Point(name='some name', description='some desc', directions='some directions',
+                  lat="15", lon="20", point_type="SHED", water_exists=True, water_comment="some water comment",
+                  fire_exists=True, fire_comment="some fire comment", created_by='some id', last_modified_by='other id')
+    assert point.last_modified_by == "other id"
+    assert point.created_by == "some id"
+
+
+def test_newPoint():
+    point = Point.new_point(name='some name', description='some desc', directions='some directions',
+                            lat="15", lon="20", point_type="SHED", water_exists=True,
+                            water_comment="some water comment",
+                            fire_exists=True, fire_comment="some fire comment", user_sub="some sub")
+    assert point.last_modified_by == "some sub"
+    assert point.created_by == "some sub"
+
+
+@pytest.fixture
+def point_from_dict():
+    body = {"_id": "7g5qqnABsqio5qhd0cbc", "_index": "wiaty_images1", "_primary_term": 1, "_seq_no": 29626, "_source":
+        {"created_timestamp": "1583403492", "created_by": "some id", "description": "EDIT: XII 2018: wiata spalona",
+         "directions": "", "fire_comment": None, "fire_exists": None, "images":
+             [{"created_timestamp": "1583403492", "name": "f660785da287e72143a5eddf77d37440.jpg", "created_by": "someone"}],
+         "location": {"lat": "50.763923", "lon": "16.180389"},
+         "name": "G\u00f3ry Wa\u0142brzyskie, masyw Che\u0142mca", "type": "SHED",
+         "water_comment": None, "water_exists": None, "last_modified_timestamp": "1583403439",
+         "last_modified_by": "other id"},
+            "_type": "_doc", "_version": 12, "found": True}
+    return Point.from_dict(body=body)
+
+
+def test_pointFromDict(point_from_dict):
+    assert point_from_dict.created_by == "some id"
+    assert point_from_dict.description == "EDIT: XII 2018: wiata spalona"
+
+
+def test_changePointName(point_from_dict):
+    changes = point_from_dict.modify(name="changed name", description=NotDefined(), directions=NotDefined(),
+                                     lat=NotDefined(), lon=NotDefined(), point_type=NotDefined(),
+                                     water_exists=NotDefined(), fire_exists=NotDefined(), water_comment=NotDefined(),
+                                     fire_comment=NotDefined(), user_sub=NotDefined())
+    assert changes == {'name': {'new_value': 'changed name', 'old_value': 'Góry Wałbrzyskie, masyw Chełmca'}}
+
+
+def test_changePointLat(point_from_dict):
+    changes = point_from_dict.modify(name=NotDefined(), description=NotDefined(), directions=NotDefined(),
+                                     lat="49", lon=NotDefined(), point_type=NotDefined(),
+                                     water_exists=NotDefined(), fire_exists=NotDefined(), water_comment=NotDefined(),
+                                     fire_comment=NotDefined(), user_sub=NotDefined())
+    assert changes == {'lat': {'new_value': '49', 'old_value': '50.763923'}}
+
+
+def test_pointToDict(point_from_dict):
+    result = point_from_dict.to_dict()
+    assert result == {"created_timestamp": "1583403492", "description": "EDIT: XII 2018: wiata spalona",
+         "directions": "", "fire_comment": None, "fire_exists": None, "images":
+             [{"created_timestamp": "1583403492", "name": "f660785da287e72143a5eddf77d37440.jpg"}],
+         "location": {"lat": "50.763923", "lon": "16.180389"},
+         "name": "G\u00f3ry Wa\u0142brzyskie, masyw Che\u0142mca", "type": "SHED",
+         "water_comment": None, "water_exists": None, "last_modified_timestamp": "1583403439"}
--- wiating_backend/elastic.py
+++ wiating_backend/elastic.py
@@ -1,13 +1,89 @@
-from copy import deepcopy
 from datetime import datetime
-from deepdiff import DeepDiff
 from elasticsearch import Elasticsearch as ES
-import re
 
 
 
 class NotDefined:
     pass
+
+
+class Point:
+    def __init__(self, name, description, directions, lat, lon, point_type, water_exists, fire_exists,
+                 created_by, last_modified_by, water_comment=None, fire_comment=None, doc_id=None,
+                 created_timestamp=None, last_modified_timestamp=None, images=None):
+        self.name = name
+        self.description = description
+        self.directions = directions
+        self.lat = str(lat)
+        self.lon = str(lon)
+        self.point_type = point_type
+        self.water_exists = water_exists
+        self.fire_exists = fire_exists
+        self.water_comment = water_comment
+        self.fire_comment = fire_comment
+        self.created_timestamp = datetime.utcnow().strftime("%s") if created_timestamp is None else created_timestamp
+        self.created_by = created_by
+        self.doc_id = doc_id
+        self.last_modified_timestamp = datetime.utcnow().strftime("%s") if last_modified_timestamp is None \
+            else last_modified_timestamp
+        self.last_modified_by = last_modified_by
+        self.images = images
+
+    @classmethod
+    def new_point(cls, name, description, directions, lat, lon, point_type, water_exists, fire_exists,
+                 user_sub, water_comment=None, fire_comment=None):
+        return cls(name=name, description=description, directions=directions, lat=lat, lon=lon, point_type=point_type,
+                   water_exists=water_exists, water_comment=water_comment, fire_exists=fire_exists,
+                   fire_comment=fire_comment, created_by=user_sub, last_modified_by=user_sub)
+    @classmethod
+    def from_dict(cls, body):
+        source = body['_source']
+        return cls(name=source['name'], description=source['description'], directions=source['directions'],
+                   lat=source['location']['lat'], lon=source['location']['lon'], point_type=source['type'],
+                   water_exists=source['water_exists'], water_comment=source['water_comment'],
+                   fire_exists=source['fire_exists'], fire_comment=source['fire_comment'],
+                   created_timestamp=source['created_timestamp'], created_by=source['created_by'],
+                   last_modified_timestamp=source['last_modified_timestamp'], last_modified_by=source['last_modified_by'],
+                   images=source.get('images'), doc_id=body['_id'])
+
+    def to_dict(self):
+        body = {
+            "name": self.name,
+            "description": self.description,
+            "directions": self.directions,
+            "location": {
+                "lat": self.lat,
+                "lon": self.lon
+            },
+            "type": self.point_type,
+            "water_exists": self.water_exists,
+            "water_comment": self.water_comment,
+            "fire_exists": self.fire_exists,
+            "fire_comment": self.fire_comment,
+            "created_timestamp": self.created_timestamp,
+            "last_modified_timestamp": self.last_modified_timestamp,
+        }
+        if self.images is not None:
+            body['images'] = list()
+            for image in self.images:
+                body["images"].append({"name": image['name'], "created_timestamp": image["created_timestamp"]})
+        return body
+
+    def modify(self, name, description, directions, lat, lon, point_type, water_exists, fire_exists, water_comment,
+               fire_comment, user_sub):
+        params = locals()
+        params.pop('self')
+        changed = dict()
+        for param in params.keys():
+            if type(params[param]) is not NotDefined:
+                if getattr(self, param) != params[param]:
+                    changed[param] = {'old_value': getattr(self, param),
+                                      'new_value': params[param]}
+                setattr(self, param, params[param])
+        self.last_modified_by = user_sub
+        self.last_modified_timestamp = datetime.utcnow().strftime("%s")
+        return changed
+
 
 
 def add_to_or_create_list(location, name, query):
@@ -22,9 +98,6 @@ class Elasticsearch:
     def __init__(self, connection_string, index='wiaty'):
         self.es = ES([connection_string])
         self.index = index
-        self.fields_to_return = ["name", "description", "directions", "location", "type", "water_exists",
-                                 "water_comment", "fire_exists", "fire_comment", "created_timestamp",
-                                 "last_modified_timestamp", "images.name", "images.created_timestamp"]
 
     def search_points(self, phrase, point_type=None, top_right=None, bottom_left=None, water=None, fire=None):
         body = {
@@ -67,8 +140,10 @@ class Elasticsearch:
                                   query={"term": {"water_exists": water}})
         if fire is not None:
             add_to_or_create_list(location=body['query']['bool'], name='filter', query={"term": {"fire_exists": fire}})
-        response = self.es.search(index=self.index, body=body, _source_includes=self.fields_to_return)
-        return {'points': response['hits']['hits']}
+        response = self.es.search(index=self.index, body=body)
+        read_points = list(map(Point.from_dict, response['hits']['hits']))
+        out_points = [point.to_dict() for point in read_points]
+        return {'points': out_points}
 
 
     def get_points(self, top_right, bottom_left):
@@ -101,32 +176,9 @@ class Elasticsearch:
         return {'points': response['hits']['hits']}
 
     def get_point(self, point_id):
-        return self.es.get(index=self.index, id=point_id, _source_includes=self.fields_to_return)
-
-    def save_backup(self, index_suffix, old_body, new_body, doc_id, user_sub):
-        def extract_key(key):
-            return re.search(r"(?<=(root\['))\w*(?='\])", key)[0]
-
-        def iterate_over_diff_output(output):
-            result = dict()
-            for item in output.keys():
-                result[extract_key(item)] = output[item]
-            return result
-
-        document = {"modified_by": user_sub, "doc_id": doc_id,
-                    "timestamp": datetime.utcnow().strftime("%Y/%m/%d %H:%M:%S"), "name": old_body['name']}
-        diff = DeepDiff(old_body, new_body, verbose_level=2,
-                        exclude_paths=["root['last_modified_timestamp']","root['last_modified_by']"])
-        if diff.get('dictionary_item_added') is not None:
-            document["added"] = iterate_over_diff_output(diff['dictionary_item_added'])
-        if diff.get('dictionary_item_removed') is not None:
-            document["removed"] = iterate_over_diff_output(diff['dictionary_item_removed'])
-        if diff.get('values_changed') is not None:
-            document["changed"] = iterate_over_diff_output(diff['values_changed'])
-        location_diff = DeepDiff(old_body['location'], new_body['location'], verbose_level=2)
-        if location_diff.get('values_changed') is not None:
-            document["changed"]["location"] = iterate_over_diff_output(location_diff['values_changed'])
-        self.es.index(index=''.join((self.index, index_suffix)), body=document)
+        response = self.es.get(index=self.index, id=point_id)
+        point = Point.from_dict(body=response)
+        return point.to_dict()
 
     def get_logs(self, point_id=None, size=25, offset=0):
         body = {"sort":[{"timestamp": {"order": "desc"}}], "from": offset, "size": size}
@@ -138,47 +190,26 @@ class Elasticsearch:
 
     def modify_point(self, point_id, user_sub, name, description, directions, lat, lon,
                      point_type, water_exists, fire_exists, water_comment, fire_comment):
-        body = self.es.get(index=self.index, id=point_id)['_source']
-        old_body = deepcopy(body)
-        body['name'] = name if type(name) != NotDefined else body['name']
-        body['description'] = description if type(description) != NotDefined else body['description']
-        body['directions'] = directions if type(directions) != NotDefined else body['directions']
-        body['location']['lat'] = str(lat) if type(lat) != NotDefined else body['location']['lat']
-        body['location']['lon'] = str(lon) if type(lon) != NotDefined else body['location']['lon']
-        body['type'] = point_type if type(point_type) != NotDefined else body['type']
-        body['water_exists'] = water_exists if type(water_exists) != NotDefined else body['water_exists']
-        body['water_comment'] = water_comment if type(water_comment) != NotDefined else body['water_comment']
-        body['fire_exists'] = fire_exists if type(fire_exists) != NotDefined else body['fire_exists']
-        body['fire_comment'] = fire_comment if type(fire_comment) != NotDefined else body['fire_comment']
-        body['last_modified_timestamp'] = datetime.utcnow().strftime("%s")
-        body['last_modified_by'] = user_sub
-        res = self.es.index(index=self.index, id=point_id, body=body)
+        body = self.es.get(index=self.index, id=point_id)
+        point = Point.from_dict(body=body)
+        changes = point.modify(name=name, description=description, directions=directions, lat=lat, lon=lon,
+                               point_type=point_type, water_exists=water_exists, water_comment=water_comment,
+                               fire_exists=fire_exists, fire_comment=fire_comment, user_sub=user_sub)
+        res = self.es.index(index=self.index, id=point_id, body=point.to_dict())
         if res['result'] == 'updated':
-            self.save_backup(datetime.today().strftime('_%m_%Y'), old_body, body, point_id, user_sub)
+            self.save_log(user_sub=user_sub, doc_id=point_id, name=point.name, changed=changes)
             return self.es.get(index=self.index, id=point_id, _source_includes=self.fields_to_return)
         return res
 
     def add_point(self, name, description, directions, lat, lon, point_type, user_sub, water_exists, fire_exists,
                   water_comment=None, fire_comment=None):
-        body = {
-            "name":  name,
-            "description": description,
-            "directions": directions,
-            "location": {
-                "lat": str(lat),
-                "lon": str(lon)
-            },
-            "type": point_type,
-            "water_exists": water_exists,
-            "water_comment": water_comment,
-            "fire_exists": fire_exists,
-            "fire_comment": fire_comment,
-            "created_timestamp": datetime.utcnow().strftime("%s"),
-            "created_by": user_sub
-        }
-        res = self.es.index(index=self.index, body=body)
+        point = Point.new_point(name=name, description=description, directions=directions, lat=lat,
+                      lon=lon, point_type=point_type, water_exists=water_exists, water_comment=water_comment,
+                      fire_exists=fire_exists, fire_comment=fire_comment, user_sub=user_sub)
+        res = self.es.index(index=self.index, body=point.to_dict())
         if res['result'] == 'created':
-            return self.es.get(index=self.index, id=res['_id'], _source_includes=self.fields_to_return)
+            self.save_log(user_sub=user_sub, doc_id=res['_id'], name=point.name, changed="created")
+            return self.get_point(point_id=res['_id'])
         return res
 
     def add_image(self, point_id, path, sub):
@@ -192,5 +223,10 @@ class Elasticsearch:
         images.append(new_image)
         res = self.es.update(index=self.index, id=point_id, body={"doc": {"images": images}})
         if res['result'] == 'updated':
-            return self.es.get(index=self.index, id=point_id, _source_includes=self.fields_to_return)
+            return self.get_point(point_id=point_id)
         return res
+
+    def save_log(self, user_sub, doc_id, name, changed):
+        document = {"modified_by": user_sub, "doc_id": doc_id, "changes": changed,
+                    "timestamp": datetime.utcnow().strftime("%Y/%m/%d %H:%M:%S"), "name": name}
+        self.es.index(index=''.join((self.index, datetime.today().strftime('_%m_%Y'))), body=document)
