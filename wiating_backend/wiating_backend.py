e1f231dcf5325d5a8762dcb08bb7bab44aaf2cb9
merito <merito123@gmail.com>
2020-09-05 22:48:13 +0200

    Remove flask_monitoringdashboard

--- wiating_backend/wiating_backend.py
+++ wiating_backend/wiating_backend.py
@@ -2,20 +2,16 @@
 
 """Python Flask Wiating API backend
 """
-import flask_monitoringdashboard as dashboard
-from os import environ as env
 from werkzeug.exceptions import HTTPException
 
 from flask import Flask, jsonify, redirect, render_template, request
 from flask_cors import CORS
 from flask_gzip import Gzip
 
-from . import constants
 from .image import images
 from .logs import logs
 from .points import points
 from .user_management import user_mgmt
-
 
 
 def configure_blueprints(app):
@@ -24,15 +20,6 @@ def configure_blueprints(app):
     with app.app_context():
         for bp in [images, logs, points, user_mgmt]:
             app.register_blueprint(bp)
-
-
-def configure_dashboard(app):
-    def group_by_user_sub():
-        return hash(request.headers.get("Authorization", None))
-
-    dashboard.config.group_by = group_by_user_sub
-    dashboard.config.init_from(file=env.get(constants.DASHBOARD_CONFIG_FILE_PATH))
-    dashboard.bind(app)
 
 
 def configure_app(app, config):
@@ -67,7 +54,6 @@ def create_app(config):
     app = Flask(__name__, static_url_path='/public', static_folder='./public')
     configure_app(app, config)
     configure_blueprints(app)
-    configure_dashboard(app)
     configure_home(app)
     configure_compression(app)
     health_check(app)
