808dfb3e182012685ce18487d1ab9e28f013026f
Dawid Wolski <merito@tuta.io>
2024-11-10 00:30:34 +0100

    Introduce keycloak

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM debian:buster
+FROM debian:bookworm
 
 RUN DEBIAN_FRONTEND=noninteractive apt-get update \
-    && apt-get install -y curl git python3-pip python-setuptools python3-dev zlib1g-dev libjpeg-dev
+    && apt-get install -y curl git python3-pip python3-setuptools python3-dev zlib1g-dev libjpeg-dev
 
 RUN mkdir -p /opt/wiating/devops
 RUN mkdir -p /images
@@ -9,7 +9,8 @@ RUN mkdir -p /images
 COPY requirements.txt .
 COPY requirements_test.txt .
 
-RUN pip3 install -r requirements_test.txt
+RUN pip3 install --break-system-packages -r requirements.txt
+RUN pip3 install --break-system-packages -r requirements_test.txt
 
 WORKDIR /opt/wiating
 
--- wiating_backend/main.py
+++ wiating_backend/main.py
@@ -1,14 +1,29 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.middleware.gzip import GZipMiddleware
-from fastapi.staticfiles import StaticFiles
-
+from wiating_backend.config import DefaultConfig
 from .image import images
 from .logs import logs
 from .points import points
 from .user_management import user_mgmt
 
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
+
+from keycloak import KeycloakOpenID
+
+
+keycloak_openid = KeycloakOpenID(server_url="http://keycloak:8080/",
+                                 client_id="wiating",
+                                 realm_name="wiating",
+                                 client_secret_key=DefaultConfig().keycloak_secret_key)
+
+config_well_known = keycloak_openid.well_known()
+
+
 app = FastAPI()
+
+
 app.include_router(images)
 app.include_router(logs)
 app.include_router(points)
@@ -33,3 +48,27 @@ app.add_middleware(GZipMiddleware)
 @app.get('/healthz')
 def healthz():
     return {}
+
+
+@app.post('/login')
+def login(username: str, password: str):
+    try:
+        token = keycloak_openid.token(username, password)
+        return token
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@app.get('/choose_identity_provider')
+def choose_identity_provider():
+    redirect_url = keycloak_openid.auth_url(redirect_uri="http://localhost:3000/")
+    return RedirectResponse(url=redirect_url)
+
+
+@app.post('/token')
+def token(code: str = Form(...)):
+    try:
+        token = keycloak_openid.token(grant_type='authorization_code', code=code, redirect_uri="http://localhost:3000/")
+        return token
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
